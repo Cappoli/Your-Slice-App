4b6719ff69d0998b72681f06b492ab3700e6e9e5
cappoli <Cappoli567@gmail.com>
2019-11-20 14:25:28 -0500

    let's go

---
+++ app/src/main/java/base/core/yourslice/MainActivity.java
@@ -0,0 +1,20 @@
+package base.core.yourslice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Button to create new person card in recycler view
+    //Field for Tax
+    //Field for Tip
+    //Variable to hold Sub-Total
+    //Field to display Total
+}
---
+++ app/src/main/java/base/core/yourslice/MainRecyclerViewAdapter.java
@@ -0,0 +1,47 @@
+package base.core.yourslice;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+
+public class MainRecyclerViewAdapter extends RecyclerView.Adapter<MainRecyclerViewAdapter.ViewHolder>{
+
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_cards, parent, AttachToRoot: false);
+        ViewHolder holder = new ViewHolder(view);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+
+    public class ViewHolder extends  RecyclerView.ViewHolder{
+
+        TextView item;
+        EditText itemCost;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+
+    }
+}
